df9c714394beb15f57aac3748709344c004647ea
Eddie Kohler <ekohler@gmail.com>
2013-02-21 09:43:21 -0500

    Add local_vector

--- lib/local_vector.hh
+++ lib/local_vector.hh
@@ -1,17 +1,17 @@
-#ifndef GSTORE_LOCAL_STACK_HH
-#define GSTORE_LOCAL_STACK_HH 1
+#ifndef GSTORE_LOCAL_VECTOR_HH
+#define GSTORE_LOCAL_VECTOR_HH 1
 #include "compiler.hh"
 #include <memory>
 
 template <typename T, int N, typename A = std::allocator<T> >
-class local_stack {
+class local_vector {
   public:
-    typedef bool (local_stack<T, N>::*unspecified_bool_type)() const;
-    typedef T *iterator;
-    typedef const T *const_iterator;
+    typedef bool (local_vector<T, N>::*unspecified_bool_type)() const;
+    typedef T* iterator;
+    typedef const T* const_iterator;
 
-    inline local_stack(const A &allocator = A());
-    inline ~local_stack();
+    inline local_vector(const A& allocator = A());
+    inline ~local_vector();
 
     inline int size() const;
     inline bool empty() const;
@@ -27,12 +27,12 @@ class local_stack {
 
     inline T& operator[](int i);
     inline const T& operator[](int i) const;
-    inline T& top();
-    inline const T& top() const;
+    inline T& back();
+    inline const T& back() const;
 
-    inline void push(const T& x);
-    inline void push(T&& x);
-    inline void pop();
+    inline void push_back(const T& x);
+    inline void push_back(T&& x);
+    inline void pop_back();
 
   private:
     struct rep : public A {
@@ -49,17 +49,17 @@ class local_stack {
 };
 
 template <typename T, int N, typename A>
-inline local_stack<T, N, A>::rep::rep(const A &a)
+inline local_vector<T, N, A>::rep::rep(const A& a)
     : A(a), v_(reinterpret_cast<T *>(lv_)), size_(0), capacity_(N) {
 }
 
 template <typename T, int N, typename A>
-inline local_stack<T, N, A>::local_stack(const A &allocator)
+inline local_vector<T, N, A>::local_vector(const A& allocator)
     : r_(allocator) {
 }
 
 template <typename T, int N, typename A>
-inline local_stack<T, N, A>::~local_stack() {
+inline local_vector<T, N, A>::~local_vector() {
     for (int i = 0; i < r_.size_; ++i)
 	r_.destroy(&r_.v_[i]);
     if (r_.v_ != reinterpret_cast<T *>(r_.lv_))
@@ -67,27 +67,27 @@ inline local_stack<T, N, A>::~local_stack() {
 }
 
 template <typename T, int N, typename A>
-inline int local_stack<T, N, A>::size() const {
+inline int local_vector<T, N, A>::size() const {
     return r_.size_;
 }
 
 template <typename T, int N, typename A>
-inline bool local_stack<T, N, A>::empty() const {
+inline bool local_vector<T, N, A>::empty() const {
     return r_.size_ == 0;
 }
 
 template <typename T, int N, typename A>
-inline local_stack<T, N, A>::operator unspecified_bool_type() const {
-    return empty() ? 0 : &local_stack<T, N, A>::empty;
+inline local_vector<T, N, A>::operator unspecified_bool_type() const {
+    return empty() ? 0 : &local_vector<T, N, A>::empty;
 }
 
 template <typename T, int N, typename A>
-inline bool local_stack<T, N, A>::operator!() const {
+inline bool local_vector<T, N, A>::operator!() const {
     return empty();
 }
 
 template <typename T, int N, typename A>
-void local_stack<T, N, A>::grow() {
+void local_vector<T, N, A>::grow() {
     T *m = r_.allocate(r_.capacity_ * 2);
     for (int i = 0; i < r_.size_; ++i) {
 	r_.construct(&m[i], std::move(r_.v_[i]));
@@ -100,57 +100,57 @@ void local_stack<T, N, A>::grow() {
 }
 
 template <typename T, int N, typename A>
-inline typename local_stack<T, N, A>::iterator local_stack<T, N, A>::begin() {
+inline typename local_vector<T, N, A>::iterator local_vector<T, N, A>::begin() {
     return r_.v_;
 }
 
 template <typename T, int N, typename A>
-inline typename local_stack<T, N, A>::iterator local_stack<T, N, A>::end() {
+inline typename local_vector<T, N, A>::iterator local_vector<T, N, A>::end() {
     return r_.v_ + r_.size_;
 }
 
 template <typename T, int N, typename A>
-inline typename local_stack<T, N, A>::const_iterator local_stack<T, N, A>::begin() const {
+inline typename local_vector<T, N, A>::const_iterator local_vector<T, N, A>::begin() const {
     return r_.v_;
 }
 
 template <typename T, int N, typename A>
-inline typename local_stack<T, N, A>::const_iterator local_stack<T, N, A>::end() const {
+inline typename local_vector<T, N, A>::const_iterator local_vector<T, N, A>::end() const {
     return r_.v_ + r_.size_;
 }
 
 template <typename T, int N, typename A>
-inline typename local_stack<T, N, A>::const_iterator local_stack<T, N, A>::cbegin() const {
+inline typename local_vector<T, N, A>::const_iterator local_vector<T, N, A>::cbegin() const {
     return r_.v_;
 }
 
 template <typename T, int N, typename A>
-inline typename local_stack<T, N, A>::const_iterator local_stack<T, N, A>::cend() const {
+inline typename local_vector<T, N, A>::const_iterator local_vector<T, N, A>::cend() const {
     return r_.v_ + r_.size_;
 }
 
 template <typename T, int N, typename A>
-inline T &local_stack<T, N, A>::operator[](int i) {
+inline T& local_vector<T, N, A>::operator[](int i) {
     return r_.v_[i];
 }
 
 template <typename T, int N, typename A>
-inline const T &local_stack<T, N, A>::operator[](int i) const {
+inline const T& local_vector<T, N, A>::operator[](int i) const {
     return r_.v_[i];
 }
 
 template <typename T, int N, typename A>
-inline T &local_stack<T, N, A>::top() {
+inline T& local_vector<T, N, A>::back() {
     return r_.v_[r_.size_ - 1];
 }
 
 template <typename T, int N, typename A>
-inline const T &local_stack<T, N, A>::top() const {
+inline const T& local_vector<T, N, A>::back() const {
     return r_.v_[r_.size_ - 1];
 }
 
 template <typename T, int N, typename A>
-inline void local_stack<T, N, A>::push(const T &x) {
+inline void local_vector<T, N, A>::push_back(const T& x) {
     if (r_.size_ == r_.capacity_)
 	grow();
     r_.construct(&r_.v_[r_.size_], x);
@@ -158,7 +158,7 @@ inline void local_stack<T, N, A>::push(const T &x) {
 }
 
 template <typename T, int N, typename A>
-inline void local_stack<T, N, A>::push(T &&x) {
+inline void local_vector<T, N, A>::push_back(T&& x) {
     if (r_.size_ == r_.capacity_)
 	grow();
     r_.construct(&r_.v_[r_.size_], std::move(x));
@@ -166,7 +166,7 @@ inline void local_stack<T, N, A>::push(T &&x) {
 }
 
 template <typename T, int N, typename A>
-inline void local_stack<T, N, A>::pop() {
+inline void local_vector<T, N, A>::pop_back() {
     --r_.size_;
 }
 
