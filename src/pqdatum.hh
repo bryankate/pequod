833548f8153fe34acebaf652ab2e1a67f9f5769a
Eddie Kohler <ekohler@gmail.com>
2013-03-04 13:22:29 -0500

    Datum to its own header

---
+++ src/pqdatum.hh
@@ -0,0 +1,76 @@
+#ifndef PEQUOD_DATUM_HH
+#define PEQUOD_DATUM_HH
+#include <boost/intrusive/set.hpp>
+#include "string.hh"
+#include "str.hh"
+
+namespace pq {
+
+typedef boost::intrusive::set_base_hook<
+    boost::intrusive::link_mode<boost::intrusive::normal_link>,
+    boost::intrusive::optimize_size<true> > pequod_set_base_hook;
+typedef boost::intrusive::set_member_hook<
+    boost::intrusive::link_mode<boost::intrusive::normal_link>,
+    boost::intrusive::optimize_size<true> > pequod_set_member_hook;
+
+class Datum : public pequod_set_base_hook {
+  public:
+    explicit Datum(const String& key)
+	: key_(key) {
+    }
+    Datum(const String& key, const String& value)
+	: key_(key), value_(value) {
+    }
+
+    const String& key() const {
+	return key_;
+    }
+    const String& value() const {
+        return value_;
+    }
+
+  private:
+    String key_;
+  public:
+    String value_;
+    pequod_set_member_hook member_hook_;
+};
+
+struct DatumCompare {
+    template <typename T>
+    inline bool operator()(const Datum& a, const String_base<T>& b) const {
+	return a.key() < b;
+    }
+    inline bool operator()(const Datum& a, Str b) const {
+	return a.key() < b;
+    }
+    template <typename T>
+    inline bool operator()(const String_base<T>& a, const Datum& b) const {
+	return a < b.key();
+    }
+    inline bool operator()(Str a, const Datum& b) const {
+	return a < b.key();
+    }
+};
+
+struct DatumDispose {
+    inline void operator()(Datum* ptr) {
+	delete ptr;
+    }
+};
+
+typedef boost::intrusive::set<Datum> ServerStore;
+
+
+inline bool operator<(const Datum& a, const Datum& b) {
+    return a.key() < b.key();
+}
+inline bool operator==(const Datum& a, const Datum& b) {
+    return a.key() == b.key();
+}
+inline bool operator>(const Datum& a, const Datum& b) {
+    return a.key() > b.key();
+}
+
+} // namespace
+#endif
