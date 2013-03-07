94f954c348a72168589c64c2f4d75c0405c8d905
Neha Narula <narula@gmail.com>
2013-03-07 15:42:19 -0500

    Pull out Hacker News populator to use with other backends

--- src/pqhackernews.hh
+++ src/pqhackernews.hh
@@ -1,56 +1,12 @@
-#ifndef PQHACKERNEWS_HH
-#define PQHACKERNEWS_HH
-#include <boost/random.hpp>
-#include <utility>
-#include <vector>
-#include <map>
-#include <set>
-#include <iostream>
-#include <stdint.h>
+#include "hnpopulator.hh"
+#include "pqjoin.hh"
+#include <sys/resource.h>
 #include "json.hh"
+#include "time.hh"
+#include "sp_key.hh"
 #include "pqserver.hh"
 
 namespace pq {
-
-class HackernewsPopulator {
-  public:
-    HackernewsPopulator(const Json& param);
-
-    inline void post_article(uint32_t author, uint32_t article);
-    inline bool vote(uint32_t author, uint32_t user);
-    inline uint32_t next_aid();
-    inline uint32_t next_comment();
-
-    inline void set_nusers(uint32_t);
-    inline uint32_t nusers() const;
-    inline uint32_t narticles() const;
-    inline void set_narticles(uint32_t n);
-    inline uint32_t karma(uint32_t author) const;
-    inline void set_log(bool val);
-    inline bool log() const;
-    inline const std::vector<uint32_t>& articles() const;
-    inline uint32_t pre() const;
-    inline uint32_t nops() const;
-    inline uint32_t vote_rate() const;
-    inline uint32_t comment_rate() const;
-    inline uint32_t post_rate() const;
-    inline bool m() const;
-
-  private:
-    Json param_;
-    bool log_;
-    uint32_t nusers_;
-    // author -> karma
-    std::vector<uint32_t> karma_;
-    // article -> author
-    std::vector<uint32_t> articles_;
-    // article -> users
-    std::map<uint32_t, std::set<uint32_t> > votes_;
-    uint32_t pre_;
-    uint32_t narticles_;
-    uint32_t ncomments_;
-    bool materialize_inline_;
-};
 
 class HackernewsRunner {
   public:
@@ -72,92 +28,281 @@ class HackernewsRunner {
     void read_tables(uint32_t aid);
 };
 
-inline uint32_t HackernewsPopulator::nusers() const {
-    return nusers_;
-}
-    
-inline void HackernewsPopulator::post_article(uint32_t author, uint32_t article) {
-    auto it = votes_.find(article);
-    mandatory_assert(it == votes_.end());
-    articles_[article] = author;
-    auto s = std::set<uint32_t>();
-    s.insert(author);
-    votes_.insert(std::pair<uint32_t, std::set<uint32_t> >(article, s));
-    ++narticles_;
-    ++karma_[author];  // one vote
-}
-
-inline bool HackernewsPopulator::vote(uint32_t article, uint32_t user) {
-    auto it = votes_.find(article);
-    mandatory_assert(it != votes_.end());    
-    if (it->second.find(user) != it->second.end())
-        return false;
-    it->second.insert(user);
-    ++karma_[articles_[article]];
-    return true;
-}
-
-inline uint32_t HackernewsPopulator::next_aid() {
-    mandatory_assert(narticles_ < articles_.size());
-    return narticles_++;
-}
-
-inline uint32_t HackernewsPopulator::next_comment() {
-    return ncomments_++;
-}
-
-inline uint32_t HackernewsPopulator::narticles() const {
-    return narticles_;
-}
-
-inline uint32_t HackernewsPopulator::pre() const {
-    return pre_;
-}
-
-inline void HackernewsPopulator::set_narticles(uint32_t n) {
-    narticles_ = n;
-}
-
-inline const std::vector<uint32_t>& HackernewsPopulator::articles() const {
-    return articles_;
-}
-
-inline uint32_t HackernewsPopulator::karma(uint32_t author) const {
-    return karma_[author];
-}
-
-inline bool HackernewsPopulator::log() const {
-    return log_;
-}
-
-inline void HackernewsPopulator::set_log(bool val) {
-    log_ = val;
-}
-
-inline uint32_t HackernewsPopulator::nops() const {
-    return param_["nops"].as_i(1000000);
-}
-
-inline uint32_t HackernewsPopulator::vote_rate() const {
-    return param_["vote_rate"].as_i(1);
-}
-
-inline uint32_t HackernewsPopulator::comment_rate() const {
-    return param_["comment_rate"].as_i(1);
-}
-
-inline uint32_t HackernewsPopulator::post_rate() const {
-    return param_["post_rate"].as_i(0);
-}
-
-inline bool HackernewsPopulator::m() const {
-    return materialize_inline_;
-}
 
 inline HackernewsRunner::HackernewsRunner(Server& server, HackernewsPopulator& hp)
     : server_(server), hp_(hp) {
 }
 
-};
+void HackernewsRunner::post_article(uint32_t author, uint32_t aid) {
+    char buf[128];
+    hp_.post_article(author, aid);
+    sprintf(buf, "a|%05d%05d", author, aid);
+    server_.insert(Str(buf, 12), Str("lalalalala", 10));
+    if (hp_.log()) {
+        printf("post %.12s\n", buf);
+    }
+    sprintf(buf, "v|%05d%05d|%05d", author, aid, author);
+    server_.insert(Str(buf, 18), Str("1", 1));
+    if (hp_.log()) {
+        printf("vote %.18s\n", buf);
+    }
+}
 
-#endif
+void HackernewsRunner::post_comment(uint32_t commentor, uint32_t aid) {
+    char buf[128];
+    uint32_t author = hp_.articles()[aid];
+    sprintf(buf, "c|%05d%05d|%05d|%05d", author, aid, hp_.next_comment(), commentor);
+    server_.insert(Str(buf, 24), Str("calalalala", 10));
+    if (hp_.log()) {
+        printf("comment  %.24s\n", buf);
+    }
+}
+
+bool HackernewsRunner::vote(uint32_t voter, uint32_t aid) {
+    char buf[128];
+    uint32_t author = hp_.articles()[aid];
+    if (hp_.vote(aid, voter)) {
+        sprintf(buf, "v|%05d%05d|%05d", author, aid, voter);
+        server_.insert(Str(buf, 18), Str("1", 1));
+        if (hp_.log()) {
+            printf("vote %.18s\n", buf);
+        }
+        return true;
+    }
+    return false;
+}
+
+void HackernewsRunner::get_karma(String user) {
+    char buf3[128];
+    sprintf(buf3, "k|%s", user.c_str());
+    auto kbit = server_.find(Str(buf3, 7));
+    uint32_t karma = 0;
+    if (kbit != NULL) {
+        karma = atoi(kbit->value().c_str());
+        uint32_t my_karma = hp_.karma(atoi(user.c_str()));
+        mandatory_assert(karma == my_karma && "Karma mismatch");
+        if (hp_.log())
+            std::cout << "  k " << user << ":" << karma << "\n";
+    }
+}
+
+void HackernewsRunner::read_article(uint32_t aid) {
+    if (hp_.m()) 
+        read_materialized(aid);
+    else
+        read_tables(aid);
+}
+
+void HackernewsRunner::read_materialized(uint32_t aid) {
+    char buf1[128], buf2[128];
+    mandatory_assert(aid < hp_.narticles());
+    uint32_t author = hp_.articles()[aid];
+    sprintf(buf1, "ma|%05d%05d|", author, aid);
+    sprintf(buf2, "ma|%05d%05d}", author, aid);
+    auto bit = server_.lower_bound(Str(buf1, 14)),
+        eit = server_.lower_bound(Str(buf2, 14));
+    for (; bit != eit; ++bit) {
+        String field = extract_spkey(2, bit->key());
+        if (hp_.log()) {
+            if (field == "a")
+                std::cout << "read " << bit->key() << ": " << bit->value() << "\n";
+            else
+                std::cout << "  " << field << " " << bit->key() << ": " << bit->value() << "\n";
+        }
+    }
+}
+
+void HackernewsRunner::read_tables(uint32_t aid) {
+    char buf1[128], buf2[128];
+    mandatory_assert(aid < hp_.narticles());
+    uint32_t author = hp_.articles()[aid];
+
+    // Article
+    sprintf(buf1, "a|%05d%05d", author, aid);
+    auto ait = server_.find(Str(buf1, 12));
+    mandatory_assert(ait != NULL);
+    if (hp_.log())
+        std::cout << "read " << ait->key() << ":" << ait->value() << "\n";
+
+    // Comments and Karma
+    sprintf(buf1, "c|%05d%05d|", author, aid);
+    sprintf(buf2, "c|%05d%05d}", author, aid);
+    auto bit = server_.lower_bound(Str(buf1, 13)),
+        eit = server_.lower_bound(Str(buf2, 13));
+    for (; bit != eit; ++bit) {
+        if (hp_.log()) {
+            std::cout << "  c " << bit->key() << ": " << bit->value() << "\n";
+            get_karma(extract_spkey(3, bit->key()));
+        }
+    }
+
+    // Votes
+    sprintf(buf1, "v|%05d%05d|", author, aid);
+    sprintf(buf2, "v|%05d%05d}", author, aid);
+    auto cit = server_.lower_bound(Str(buf1, 13)),
+        ceit = server_.lower_bound(Str(buf2, 13));
+    uint32_t votect = 0;
+    for (; cit != ceit; ++cit) {
+        votect++;
+        if (hp_.log())
+            std::cout << "  v " << cit->key() << ": " << cit->value() << "\n";
+    }
+}
+
+void HackernewsRunner::populate() {
+    boost::mt19937 gen;
+    gen.seed(13918);
+    boost::random_number_generator<boost::mt19937> rng(gen);
+    uint32_t nv = 0;
+    uint32_t nc = 0;
+    
+    for (uint32_t aid = 0; aid < hp_.pre(); aid++) {
+        const uint32_t author = rng(hp_.nusers());
+        post_article(author, aid);
+        const uint32_t ncomment = rng(10);
+        for (uint32_t j = 1; j <= ncomment; ++j) {
+            nc++;
+            const uint32_t commentor = rng(hp_.nusers());
+            post_comment(commentor, aid);
+        }
+        const uint32_t nvote = rng(10);
+        for (uint32_t j = 0; j < nvote; ++j) {
+            const uint32_t voter = rng(hp_.nusers());
+            if (vote(voter, aid))
+                nv++;
+        }
+    }
+
+    String start, end, join_str;
+    bool valid;
+    pq::Join* j;
+
+    if (hp_.m()) {
+        start = "ma|";
+        end = "ma}";
+        // Materialize articles
+        join_str = "ma|<author:5><seqid:5>|a "
+            "a|<author><seqid> ";
+        j = new pq::Join;
+        valid = j->assign_parse(join_str);
+        mandatory_assert(valid && "Invalid ma|article join");
+        server_.add_join(start, end, j);
+        
+        // Materialize votes
+        join_str = "ma|<author:5><seqid:5>|v "
+            "v|<author><seqid>|<voter:5> ";
+        j = new pq::Join;
+        valid = j->assign_parse(join_str);
+        mandatory_assert(valid && "Invalid ma|votes join");
+        j->set_jvt(jvt_count_match);
+        server_.add_join(start, end, j);
+
+        // Materialize comments
+        join_str = "ma|<author:5><seqid:5>|c|<cid:5>|<commenter:5> "
+            "c|<author><seqid>|<cid>|<commenter> ";
+        j = new pq::Join;
+        valid = j->assign_parse(join_str);
+        mandatory_assert(valid && "Invalid ma|comments join");
+        server_.add_join(start, end, j);
+        
+        // Materialize karma inline
+        std::cout << "Materializing karma inline with articles.\n";
+        join_str = "ma|<aid:10>|k|<cid:5>|<commenter:5> "
+            "c|<aid>|<cid>|<commenter> "
+            "v|<commenter><seq:5>|<voter:5>";
+        j = new pq::Join;
+        valid = j->assign_parse(join_str);
+        mandatory_assert(valid && "Invalid ma|karma join");
+        j->set_jvt(jvt_count_match);
+        start = "ma|";
+        end = "ma}";
+        server_.add_join(start, end, j);
+    } else {
+        // Materialize karma in a separate table
+        j = new pq::Join;
+        join_str = "k|<author:5> "
+            "a|<author:5><seqid:5> "
+            "v|<author><seqid>|<voter:5>";
+        start = "k|";
+        end = "k}";
+        valid = j->assign_parse(join_str);
+        mandatory_assert(valid && "Invalid karma join");
+        j->set_jvt(jvt_count_match);
+        server_.add_join(start, end, j);
+    }
+
+    std::cout << "Added " << hp_.nusers() << " users, " << hp_.narticles() 
+              << " articles, " << nv << " votes, " << nc << " comments." << std::endl;
+}
+
+void HackernewsRunner::run() {
+    boost::mt19937 gen;
+    gen.seed(13918);
+    boost::random_number_generator<boost::mt19937> rng(gen);
+    const uint32_t nops = hp_.nops();
+    const uint32_t nusers = hp_.nusers();
+    struct rusage ru[2];
+    uint32_t nread = 0, npost = 0, ncomment = 0, nvote = 0;
+
+    char buf1[128], buf2[128];
+    sprintf(buf1, "k|");
+    sprintf(buf2, "k}");
+    server_.validate(Str(buf1, 2), Str(buf2, 2));
+    if (hp_.log()) {
+        std::cout << ": karma scan [" << buf1 << "," << buf2 << ")\n";
+        auto bit = server_.lower_bound(Str(buf1, 2)),
+            eit = server_.lower_bound(Str(buf2, 2));
+        for (; bit != eit; ++bit)
+            std::cout << "  " << bit->key() << ": " << bit->value() << "\n";
+        std::cout << ": end karma scan [" << buf1 << "," << buf2 << ")\n";
+        std::cout << ": my karma table:\n";
+        for (int i = 0; i < hp_.nusers(); i++)
+            std::cout << " k " << i << ": " << hp_.karma(i) << "\n";
+        std::cout << ": end my karma table\n";
+    }
+
+    sprintf(buf1, "ma|");
+    sprintf(buf2, "ma}");
+    server_.validate(Str(buf1, 3), Str(buf2, 3));
+
+    if (hp_.log()) {
+        std::cout << ": ma scan [" << buf1 << "," << buf2 << ")\n";
+        auto bit = server_.lower_bound(Str(buf1, 3)),
+            eit = server_.lower_bound(Str(buf2, 3));
+        for (; bit != eit; ++bit)
+            std::cout << "  " << bit->key() << ": " << bit->value() << "\n";
+        std::cout << ": end ma scan [" << buf1 << "," << buf2 << ")\n";
+    }
+
+    std::cout << "Finished validate.\n";
+    getrusage(RUSAGE_SELF, &ru[0]);
+    for (uint32_t i = 0; i < nops; ++i) {
+        uint32_t p = rng(100);
+        uint32_t user = rng(nusers);
+        if (p < hp_.post_rate()) {
+            post_article(user, hp_.next_aid());
+            npost++;
+        } else {
+            uint32_t aid = rng(hp_.narticles());
+            read_article(aid);
+            nread++;
+            if (p < hp_.vote_rate()) {
+                vote(user, aid);
+                nvote++;
+            }
+            if (p < hp_.comment_rate()) {
+                post_comment(user, aid);
+                ncomment++;
+            }
+        }
+    }
+    getrusage(RUSAGE_SELF, &ru[1]);
+    Json stats = Json().set("nread", nread).set("npost", npost)
+	.set("ncomment", ncomment).set("nvote", nvote)
+	.set("time", to_real(ru[1].ru_utime - ru[0].ru_utime));
+    stats.merge(server_.stats());
+    std::cout << stats.unparse(Json::indent_depth(4)) << "\n";
+
+}
+
+}
