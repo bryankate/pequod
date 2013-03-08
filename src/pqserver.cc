f569ed261325d6923ff3fd6d9d7b8a802fe182da
Eddie Kohler <ekohler@gmail.com>
2013-03-08 08:48:28 -0500

    Valgrind complained

--- src/pqserver.cc
+++ src/pqserver.cc
@@ -319,8 +319,8 @@ static Clp_Option options[] = {
 enum { mode_unknown, mode_twitter, mode_hn, mode_facebook, mode_analytics, mode_listen, mode_tests };
 
 int main(int argc, char** argv) {
-    String envstr("TAMER_NOLIBEVENT=1");
-    putenv(envstr.mutable_data());
+    char envstr[] = "TAMER_NOLIBEVENT=1";
+    putenv(envstr);
     tamer::initialize();
 
     int mode = mode_unknown, listen_port = 8000, client_port = -1;
