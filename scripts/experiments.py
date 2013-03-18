a506a2afd78a4022cc72c33c7f59099c46ecf7c1
Yandong Mao <ydmao@mit.edu>
2013-03-18 14:58:43 -0400

    rwmicro2 is not needed for now

--- scripts/experiments.py
+++ scripts/experiments.py
@@ -2,7 +2,6 @@
 import copy
 
 rwmicro = []
-rwmicro2 = []
 cmdbase = "./obj/pqserver --nusers=2000 --nops=2000000 --rwmicro --nfollower=20 "
 prefresh = [0, 5, 10, 20, 40, 60, 80, 90, 95, 100]
 for pr in prefresh:
@@ -14,17 +13,8 @@ for pr in prefresh:
         {'plotgroup': "pull",
          'plotkey' : pr,
          'cmd': "%s --prefresh=%d --no-push" % (cmdbase, pr)})
-    rwmicro2.append(
-        {'plotgroup': "push",
-         'plotkey' : pr,
-         'cmd': "%s --prefresh=%d --push --prerefresh" % (cmdbase, pr)})
-    rwmicro2.append(
-        {'plotgroup': "pull",
-         'plotkey' : pr,
-         'cmd': "%s --prefresh=%d --no-push --prerefresh" % (cmdbase, pr)})
 
 
 exps = [{'name': "rwmicro", 'defs': rwmicro, 'xlabel' : 'refresh ratio (%)'},
-        {'name': "rwmicro2", 'defs': rwmicro2, 'xlabel' : 'refresh ratio (%)'},
         ]
 
