094caad71eeee1f7fb6e1a7f81b2dc44962adcff
Bryan Kate <bkate@eecs.harvard.edu>
2013-03-20 00:17:09 -0400

    quit after a fixed number of posts

--- src/pqserver.cc
+++ src/pqserver.cc
@@ -232,6 +232,7 @@ static Clp_Option options[] = {
     { "overhead", 0, 4009, 0, Clp_Negate },
     { "celebrity", 0, 4010, Clp_ValInt, 0 },
     { "celebrity2", 0, 4011, Clp_ValInt, 0 },
+    { "postlimit", 0, 4012, Clp_ValInt, 0 },
 
     // mostly HN params
     { "narticles", 'a', 5000, Clp_ValInt, 0 },
@@ -345,6 +346,8 @@ int main(int argc, char** argv) {
             tp_param.set("celebrity", clp->val.i);
         else if (clp->option->long_name == String("celebrity2"))
             tp_param.set("celebrity", clp->val.i).set("celebrity_type", 2);
+        else if (clp->option->long_name == String("postlimit"))
+            tp_param.set("postlimit", clp->val.i);
 
         // hn
 	else if (clp->option->long_name == String("narticles"))
--- src/pqtwitternew.tcc
+++ src/pqtwitternew.tcc
@@ -62,6 +62,7 @@ bool TwitterUser::Compare::operator() (const uint32_t& a, const TwitterUser& b) 
 TwitterNewPopulator::TwitterNewPopulator(const Json& param)
     : nusers_(param["nusers"].as_i(5000)),
       duration_(param["duration"].as_i(100000)),
+      postlimit_(param["postlimit"].as_i(0)),
       push_(param["push"].as_b(false)),
       pull_(param["pull"].as_b(false)),
       log_(param["log"].as_b(false)),
