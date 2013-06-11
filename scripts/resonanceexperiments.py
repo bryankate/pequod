29429bb9d8d94a95a864bb5e9953df68130acb53
Bryan Kate <bkate@eecs.harvard.edu>
2013-06-11 12:06:39 -0400

    adding populate command

--- scripts/resonanceexperiments.py
+++ scripts/resonanceexperiments.py
@@ -2,20 +2,25 @@
 import copy
 
 exps = [{'name': "twitter", 'defs': []}]
+graphfile = "twitter_graph_22M.dat"
 
-serverCmd = "./obj/pqserver"
-clientCmd = "./obj/pqserver --twitternew --plogout=0 --psubscribe=0 --duration=10000000 --popduration=0 --graph=twitter_graph_22M.dat"
+serverCmd   = "./obj/pqserver"
+populateCmd = "./obj/pqserver --twitternew --no-execute --graph=%s" % (graphfile)
+clientCmd   = "./obj/pqserver --twitternew --plogout=0 --psubscribe=0 --duration=10000000 --popduration=0 --graph=%s" % (graphfile)
+
 
 exps[0]['defs'].append(
     {'name': "autopush",
      'def_part': "twitternew",
      'servercmd': serverCmd,
-     'clientcmd': "%s" % (clientCmd)})
+     'populatecmd': populateCmd,
+     'clientcmd': clientCmd})
 
 '''
 exps[0]['defs'].append(
     {'name': "push",
      'def_part': "twitternew",
      'servercmd': serverCmd,
+     'populatecmd': populateCmd,
      'clientcmd': "%s --push" % (clientCmd)})
 '''
