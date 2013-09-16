0609ada123682cf43fbf36b55deaf7268272eddc
Bryan Kate <bkate@eecs.harvard.edu>
2013-09-16 09:16:49 -0400

    removing resonance script. use amazon

--- add_exports.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH="/home/kester/local/lib64:/home/kester/local/lib:$LD_LIBRARY_PATH"
-export LDFLAGS="-L/home/kester/local/lib64 -L/home/kester/local/lib -L/home/kester/source/boost_1_53_0/libs"
-export CPPFLAGS="-I/home/kester/local/include -I/home/kester/source/boost_1_53_0"
-export PATH="/home/kester/local/bin:$PATH"
