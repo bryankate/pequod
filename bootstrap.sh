81954734f1989edaf64e919b92500076bf99508a
Bryan Kate <bkate@eecs.harvard.edu>
2014-04-08 11:40:52 -0400

    add memtier to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-if [ ! -f tamer/boostrap.sh ]; then
+# check to see if we have the submodules checked out
+if [ ! -f tamer/boostrap.sh ] || [ ! -f memtier_benchmark/README.md ]; then
     git submodule init
     git submodule update
 fi
